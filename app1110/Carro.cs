af5464a465848a3dabf4718fad17c48720ee4d99
alberto <albertobarbosa0003@gmail.com>
2022-04-22 14:05:07 -0300

    Adicionar arquivos de projeto.

---
+++ app1110/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace app1110
+{
+    internal class Carro
+    {
+        public string NomeDoCarro { get; set; } = default!;
+        public int NumeroDaPlaca { get; set; }
+        public string CorDoCarro { get; set; } = default!;
+
+        public string CarroNome()
+        {
+            return NomeDoCarro;
+        }
+        public int PlacaNumero()
+        {
+            return NumeroDaPlaca;
+        }
+        public string Cores()
+        {
+            return CorDoCarro;
+        }
+    }
+}
---
+++ app1110/CarroCompleto.cs
@@ -0,0 +1,18 @@
+﻿using app1110;
+using System.IO;
+
+Carro carro = new Carro();
+
+carro.NomeDoCarro = Console.ReadLine();
+carro.NumeroDaPlaca = int.Parse(Console.ReadLine());
+carro.CorDoCarro = Console.ReadLine();
+
+string[] lines = { "Carro : " + carro.NomeDoCarro, "Placa : " + carro.NumeroDaPlaca, "Cor : " + carro.CorDoCarro };
+
+string docPath = @"C:\Users\Àlberto Barbòsa\Desktop\PastaImprime";
+
+using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
+{
+    foreach (string line in lines)
+        outputFile.WriteLine(line);
+}
